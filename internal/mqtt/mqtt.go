bd9cdd603bbbcd8ae7fb260fe96fc1b934328f62
Mirza Hilmi <dev@mrz.my.id>
2025-01-03 22:33:56 +0700

    feat(mqtt): enable autoreconnect & set keep alive and timeout duration

--- internal/mqtt/mqtt.go
+++ internal/mqtt/mqtt.go
@@ -2,6 +2,7 @@ package mqtt
 
 import (
 	"fmt"
+	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/lab-icn/water-potability-sensor-service/internal/config"
@@ -16,10 +17,24 @@ func NewClient(cfg *config.Config, log *zerolog.Logger) (mqtt.Client, error) {
 			cfg.MQTT.Host,
 			cfg.MQTT.Port,
 		)).
+		SetClientID(cfg.MQTT.ClientID).
 		SetUsername(cfg.MQTT.Username).
 		SetPassword(cfg.MQTT.Password).
-		SetClientID(cfg.MQTT.ClientID).
-		SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {})
+		SetKeepAlive(15 * time.Second).
+		SetPingTimeout(10 * time.Second).
+		SetAutoReconnect(true).
+		SetCleanSession(false).
+		SetDefaultPublishHandler(func(_ mqtt.Client, _ mqtt.Message) {}).
+		SetConnectionLostHandler(func(_ mqtt.Client, _ error) {
+			log.Warn().Msg("mqtt connection lost")
+		}).
+		SetReconnectingHandler(func(_ mqtt.Client, _ *mqtt.ClientOptions) {
+			log.Info().Msg("mqtt reconnecting")
+		}).
+		SetOnConnectHandler(func(_ mqtt.Client) {
+			log.Info().Msg("mqtt connected")
+		})
+
 	client := mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		return nil, token.Error()
